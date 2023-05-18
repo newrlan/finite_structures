f5f53152a07eab215ae95622abfe98fab3f24b16
newrlan <nonZeroDeterminant@gmail.com>
2023-05-18 09:52:20 +0700

    show

--- src/rubik/representations.py
+++ src/rubik/representations.py
@@ -61,13 +61,13 @@ class InvoluteRepresentation(Representation):
     Note: Пустые строки в файле будут игнорироваться.
     """
 
-    colors = [col.name.lower() for col in Color]
-    codes = [f'{c}{n}' for n in range(1, 8 + 1) 
-             for c in [col.name.lower() for col in Color]]
+    colors = [col.name.upper() for col in Color]
+    codes = [f'{c}{n}' for n in range(1, 9 + 1) 
+             for c in [col.name.upper() for col in Color]]
 
     def __init__(self, path: Optional[Path] = None):
-        if path is not None:
-            self = self.load(path)
+
+        self.state = {key: key[0] for key in self.codes}
 
     @staticmethod
     def _square_prepare(square: List[List[str]]) -> dict[str, str]:
@@ -76,7 +76,6 @@ class InvoluteRepresentation(Representation):
         assert c.upper() in Color.__members__, f"Undefined color {c.upper()}."
         res = dict()
         line = sum(square, [])
-        line.pop(4)
         for i, col in enumerate(line):
             assert col.upper() in Color.__members__, f"Undefined color {col.upper()}"
             res[f'{c}{i+1}'] = col
@@ -89,14 +88,22 @@ class InvoluteRepresentation(Representation):
         error_msg = "Incorrect coloring representation."
         assert len(coloring) == 19, error_msg
 
+        # В первой строке лежат координаты векторов Ox, Oy, Oz (положительные направления)
         squares = []
         for i in range(6):
             squares.append(coloring[i+1:i+4])
 
         add_msg = " Please, check orientation."
-        assert squares[0][1][1].lower() == coloring[0][0].lower, error_msg + add_msg
-        assert squares[1][1][1].lower() == coloring[1][0].lower, error_msg + add_msg
-        assert squares[3][1][1].lower() == coloring[2][0].lower, error_msg + add_msg
+        assert squares[0][1][1].lower() == coloring[0][2].lower, error_msg + add_msg
+        assert squares[1][1][1].lower() == coloring[0][0].lower, error_msg + add_msg
+        assert squares[3][1][1].lower() == coloring[0][1].lower, error_msg + add_msg
+
+        # В следующих переменных содержатся оппозиционные цвета. Первый цвет в
+        # паре, цвет положительного направления, второй - отрицательного.
+
+        self._color_Ox = (coloring[0][0], squares[1][1][1])
+        self._color_Oy = (coloring[0][1], squares[5][1][1])
+        self._color_Oz = (coloring[0][2], squares[2][1][1])
 
         state = dict()
         for sqr in squares:
@@ -113,12 +120,86 @@ class InvoluteRepresentation(Representation):
             'W': 'white',
             'G': 'green',
             'R': 'red',
-            'B': 'blue',
-            'Y': 'yellow',
-            'O': 'orrange'
+            'B': 'light_cyan',
+            'Y': 'light_yellow',
+            'O': 'yellow'
+            # 'O': 'grey'
         }
         val = self.state[key]
         return colored('栗', color[val])
+
+    def _show_face(self, k: str) -> List[str]:
+        if k not in self.colors:
+            raise ValueError(f'Key {k} must be in list {self.colors}')
+
+        res = []
+        for j in range(3):
+            line = ''.join([self._show_label(f'{k}{i+3*j}') for i in [1, 2, 3]])
+            res.append(line)
+
+        return res
+        # return '\n'.join(res)
+
+    def show(self):
+        """ Показать раскраску на развертке куба.
+        Схема как будут расположены квадраты развертки:
+                    Oz
+            -Ox -Oy Ox Oy
+                   -Oz
+        """
+
+        i_matrix = [
+            '                ╭───────╮     ',
+            '                │ 123│     ',
+            '                │ 456│     ',
+            '                │ 789│     ',
+            '╭───────┬───────┼───────┼───────╮',
+            '│ 369│ 369│ 123│ 369│',
+            '│ 258│ 258│ 456│ 258│',
+            '│ 147│ 147│ 789│ 147│',
+            '╰───────┴───────┼───────┼───────╯',
+            '                │ 123│     ',
+            '                │ 456│     ',
+            '                │ 789│     ',
+            '                ╰───────╯     ',
+                ]
+
+        c_matrix = [
+            '                ╭───────╮     ',
+            '                │ OOO│     ',
+            '                │ OOO│     ',
+            '                │ OOO│     ',
+            '╭───────┬───────┼───────┼───────╮',
+            '│ GGG│ WWW│ BBB│ YYY│',
+            '│ GGG│ WWW│ BBB│ YYY│',
+            '│ GGG│ WWW│ BBB│ YYY│',
+            '╰───────┴───────┼───────┼───────╯',
+            '                │ RRR│     ',
+            '                │ RRR│     ',
+            '                │ RRR│     ',
+            '                ╰───────╯     ',
+                ]
+
+        res = []
+        for c_line, i_line in zip(c_matrix, i_matrix):
+            colored_line = ''
+            for c, i in zip(c_line, i_line):
+                try:
+                    elem = self._show_label(c + i)
+                except KeyError:
+                    elem = c
+                colored_line += elem
+
+            res.append(colored_line)
+
+        print('\n'.join(res))
+        return None
+
+        
+
+
+
+        
 
     # def show_square(self, color: Color):
     # def show_square(self):
@@ -154,4 +235,4 @@ class InvoluteRepresentation(Representation):
 
 if __name__ == '__main__':
     cl = InvoluteRepresentation()
-    cl.show_square()
+    cl.show()
